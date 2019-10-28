16d888f8e498601a146a8a07a044b96f71df7904
杨锋 <896148932@qq.com>
2019-10-28 10:47:25 +0800

    更改eslintrc.js文件配置，解决行前因为空格出现的错误提示

--- demo/.eslintrc.js
+++ demo/.eslintrc.js
@@ -11,7 +11,7 @@ module.exports = {
   extends: [
     // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
     // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
-    'plugin:vue/essential', 
+    'plugin:vue/essential',
     // https://github.com/standard/standard/blob/master/docs/RULES-en.md
     'standard'
   ],
@@ -21,9 +21,19 @@ module.exports = {
   ],
   // add your custom rules here
   rules: {
+    // 'vue/script-indent': ['error', 2, {'baseIndent': 1}], // 单独设置每行前的空格
+    // 'space-before-blocks': [0, 'always'], //不以新行开始的块{前面要不要有空格
     // allow async-await
     'generator-star-spacing': 'off',
     // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
-  }
+  },
+  'overrides': [ // 关闭行前错误警告
+    {
+      'files': ['*.vue'],
+      'rules': {
+        'indent': 'off'
+      }
+    }
+  ]
 }
